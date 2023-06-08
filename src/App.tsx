0a06fd98eeb1775f7499358f8a1a90e8424482a7
smp6340 <smp6340@naver.com>
2023-06-08 14:05:26 +0900

    route :: Profile Page Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MainPage from './pages/main';
+import MyProfilePage from './pages/profile';
 import PopularityPage from './pages/community/popularity';
 import LatestPage from './pages/community/latest';
 
@@ -11,6 +11,7 @@ function App(): React.ReactElement {
       <Routes>
         <Route path='/community/popularity' element={<PopularityPage />} />
         <Route path='/community/latest' element={<LatestPage />} />
+        <Route path='/profile' element={<MyProfilePage />} />
       </Routes>
     </Router>
     </>
